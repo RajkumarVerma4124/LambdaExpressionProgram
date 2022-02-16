d1293fa0711afb7cb8ad08a888c9d3b5fec338f9
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-16 11:17:28 +0530

    [Raj] Added:- The list object to create default person details

---
+++ LamdaExpressionProgram/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamdaExpressionProgram
+{
+    /// <summary>
+    /// Created The Person Class With SSN, Name, Address, Age(UC1)
+    /// </summary>
+    public class Person
+    {
+        //Declaring the properties(UC1)
+        public int SSN { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+
+        //Overiding the string method (UC1)
+        public override string ToString()
+        {
+            return $"Person SSN : {SSN} \tName : {Name} \tAddress : {Address} \tAge : {Age}";
+        }
+    }
+}
--- LamdaExpressionProgram/Program.cs
+++ LamdaExpressionProgram/Program.cs
@@ -9,11 +9,41 @@ namespace LamdaExpressionProgram
     public class Program
     {
         /// <summary>
-        /// Person Data Management Using Lambda Function
+        /// Person Data Management Using Lambda Expression
         /// </summary>
         public static void Main(string[] args)
         {
+            //Displaying the message
             Console.WriteLine("Welcome To The Person Data Management Using Lambda Expression Program");
+            
+            //Creating the list object(UC1)
+            List<Person> list = new List<Person>();
+            //Calling the method to add default values to the list(UC1)
+            Console.WriteLine("\nPerson Details Are Listed Below");
+            AddPersonDetailsIntoList(list);
+            Console.ReadLine();
+        }
+
+        //Method to add the default values to create list
+        public static void AddPersonDetailsIntoList(List<Person> list)
+        {
+            list.Add(new Person() { Address = "Mumbai", Age = 15, Name = "Raj", SSN = 1 });
+            list.Add(new Person() { Address = "Delhi", Age = 14, Name = "Yash", SSN = 2 });
+            list.Add(new Person() { Address = "Mumbai", Age = 32, Name = "Ajay", SSN = 3 });
+            list.Add(new Person() { Address = "Mumbai", Age = 65, Name = "Raj", SSN = 4 });
+            list.Add(new Person() { Address = "Faridabad", Age = 70, Name = "Mansi", SSN = 5 });
+            list.Add(new Person() { Address = "Bhopal", Age = 62, Name = "Nidhi", SSN = 6 });
+            list.Add(new Person() { Address = "Kerala", Age = 17, Name = "Aman", SSN = 7 });
+            IterateOverList(list);
+        }
+
+        //Method to iterate over the list(UC1)
+        public static void IterateOverList(List<Person> list)
+        {
+            foreach (Person person in list)
+            {
+                Console.WriteLine(person);
+            }
             Console.ReadLine();
         }
     }
