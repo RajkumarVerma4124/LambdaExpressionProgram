01dfe4bd362f82cbdb52b62b4ad6967d41c63d5c
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-16 11:07:58 +0530

    [Raj] Added:- The welcome message in the main program

---
+++ LamdaExpressionProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamdaExpressionProgram
+{
+    public class Program
+    {
+        /// <summary>
+        /// Person Data Management Using Lambda Function
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Person Data Management Using Lambda Expression Program");
+            Console.ReadLine();
+        }
+    }
+}
